caba3188ee49bace2d3d01e12da413e864d1f946
hieuphampm <hieuphamminh976@gmail.com>
2025-03-03 09:10:22 +0700

    update ios

---
+++ IOS/Shared/IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSApp.swift
+//  Shared
+//
+//  Created by Hieu on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
