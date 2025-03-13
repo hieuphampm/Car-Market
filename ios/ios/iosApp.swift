d70009dd7e2e692ba10a7975ee10384124536bc0
hieuphampm <hieuphamminh976@gmail.com>
2025-03-13 13:14:20 +0700

    create ios

---
+++ ios/ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by umtlab03im13 on 13/3/25.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
