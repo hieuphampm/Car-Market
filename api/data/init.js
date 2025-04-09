e7ab28e3f297a73885013f2f4b9332f8f492266e
Phuoc Hieu <hieuphamminh976@gmail.com>
2025-04-09 07:15:22 +0700

    up

--- api/data/init.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
-
-export async function initDatabase() {
-	const DATABASE_URL = process.env.DATABASE_URL;
-	await mongoose.connect(DATABASE_URL );
-	console.log(`Database is up and running at: ${DATABASE_URL }`);
-}
